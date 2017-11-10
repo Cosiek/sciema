7c4986e8584cc5f2dc18c12cbc870f2b841930a1
cosiek <witold.majchrzak@21s.pl>
2017-11-10 14:43:28 +0100

    Server hello world

---
+++ sciema/server.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import os
+import socket
+
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
+from tornado.options import define, options, parse_command_line
+
+
+define('port', default=8888, help=u'Port na którym serwer ma działać', type=int)
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self):
+        self.render("home.html")
+
+
+app = tornado.web.Application(
+    [(r'/', IndexHandler),],
+    template_path=os.path.join(os.path.dirname(__file__), "templates")
+)
+
+
+if __name__ == '__main__':
+    # check current ip:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
+
+    parse_command_line()
+    print(u'Zaczynam pracę na {}:{}'.format(s.getsockname()[0], options.port))
+    app.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
